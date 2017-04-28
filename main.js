b2907311c40f2efa19aadc1b69dee9ec0f2d1952
Alex Epstein <epsteina@wit.edu>
2017-04-28 09:40:07 -0400

    Formatting & Comments

--- main.js
+++ main.js
@@ -1,56 +1,54 @@
-const {app, Tray, Menu, BrowserWindow} = require('electron');
-const path = require('path');
-const url = require('url');
-const iconPath = path.join(__dirname, 'icon.png');
-let tray = null;
-let win = null;
+const {app, Tray, Menu, BrowserWindow} = require('electron'); //electron application stuff
+const path = require('path'); //allows for use of path
+const url = require('url'); //allows for loadURL and url.format
+const iconPath = path.join(__dirname, 'icon.png'); //grab the icon
+let tray = null; //set the tray to null
+let win = null; //set the main window to null
 
 app.on('ready', function(){
-  win = new BrowserWindow({width: 600, height: 400, resizable: false});
+  win = new BrowserWindow({width: 600, height: 400, resizable: false}); //create main window
 
-  win.setMenu(null);
-  win.loadURL(url.format({
+  win.setMenu(null); //the main window had no menu
+  win.loadURL(url.format({    //loads the webpage for the main window
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
-  win.openDevTools();
-  win.on('minimize',function(event){
+  win.openDevTools(); //starts the application with developer tools open
+
+  win.on('minimize',function(event){ //prevents standard minimize function of a main window
         event.preventDefault()
             win.hide();
     });
-
-
-
-  win.on('close', function (event) {
+  win.on('close', function (event) { //prevents the closing of the aplication when the window closes
         if( !app.isQuiting){
             event.preventDefault()
             win.hide();
         }
         return false;
     });
-  tray = new Tray(iconPath);
-  var contextMenu = Menu.buildFromTemplate([
+  tray = new Tray(iconPath); //create a new tray
+  var contextMenu = Menu.buildFromTemplate([  //start buliding out the menu for the tray
 
-          { label: 'Show App', click:  function(){
+          { label: 'Insomnia', click:  function(){  //makes the main window reappear
               win.show();
           } },
-          { label: 'About Insomnia', click:  function(){
+          { label: 'About', click:  function(){ //shows the about window
             abt = new BrowserWindow({width: 400, height: 400, resizable: false});
-            abt.setMenu(null);
-            abt.loadURL(url.format({
+            abt.setMenu(null); //the about window has no menu
+            abt.loadURL(url.format({  //loads the webpage for the about window
               pathname: path.join(__dirname, 'about.html'),
               protocol: 'file:',
               slashes: true
             }))
 
           } },
-          { label: 'Quit', click:  function(){
+          { label: 'Quit', click:  function(){ //quit the application
               app.isQuiting = true;
-              app.quit();
+              app.quit(); //quit called
 
           } }
       ]);
-  tray.setToolTip('Insomnia');
-  tray.setContextMenu(contextMenu);
+  tray.setToolTip('Insomnia'); //Honestly no clue but itll make the tray say insomnia in some other place
+  tray.setContextMenu(contextMenu); //attach the menu to the tray
 });
