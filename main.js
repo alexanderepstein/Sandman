2ccacf0c6681dd433fa479b185674162df06a1df
Alex Epstein <epsteina@wit.edu>
2017-04-27 20:15:06 -0400

    Adding notifications & chron jobs
    Former-commit-id: 623c4205476983d03c2acef3eb415dee27d6b136 [formerly 852c85d4a8d1d777b26bd3c10eca0d607ee8ef8a]
    Former-commit-id: fabf314671d5f53648bc5769f593de35c3edd679

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const {app, Tray, Menu, BrowserWindow} = require('electron');
 const path = require('path');
-const url = require('url')
+const url = require('url');
 const iconPath = path.join(__dirname, 'icon.png');
 let tray = null;
 let win = null;
