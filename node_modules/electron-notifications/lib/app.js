df0be5fddfde92834c085f5d9772013eb5308c41
Alex Epstein <epsteina@wit.edu>
2017-05-02 03:20:21 -0400

    The npm install command grabs all these modules so i took them off

--- node_modules/electron-notifications/lib/app.js
+++
@@ -1,7 +0,0 @@
-const Notification = require('../notification')
-
-const { ipcRenderer } = require('electron')
-
-ipcRenderer.on('setup', (event, title, options) => {
-  new Notification(title, options)
-})
