dbf8327913e30a87895e881af439a46238e20e4e
Alexander Epstein <epsteina@WITR90GME8Q.wit.private>
2017-05-05 03:38:02 -0400

    Fixed closeOnX Bug & made windows larger
    Former-commit-id: d74c024755c2c1fa02f8c6452ddfe7c30c57a408 [formerly b918de811e472de908c53eb8b754dd72cccac32d]
    Former-commit-id: b33344be3b6e3ae8e80a8d1089abfbfd60baf20d

--- main.js
+++ main.js
@@ -3,16 +3,18 @@ const path = require('path'); //allows for use of path
 const url = require('url'); //allows for loadURL and url.format
 const iconPath = path.join(__dirname, 'icon.png'); //grab the icon
 const settings = require('electron-settings');
+const fs = require('fs');
+const filePath = path.join(__dirname, 'settings.txt');
 let tray = null; //set the tray to null
 let win = null; //set the main window to null
 let pref = null;
 let abt = null;
 var closeOnX = false;
-
+var mySettings = [];
 
 
 app.on('ready', function(){
-  win = new BrowserWindow({width: 600, height: 450, resizable: false}); //create main window
+  win = new BrowserWindow({width: 600, height: 475, resizable: false}); //create main window
 
   win.setMenu(null); //the main window had no menu
   win.loadURL(url.format({    //loads the webpage for the main window
@@ -25,23 +27,29 @@ app.on('ready', function(){
 
 
   //console.log(settings.getAll());
-  //win.openDevTools(); //starts the application with developer tools open
+  win.openDevTools(); //starts the application with developer tools open
 
 
   getCloseOnXPref();
-if (!closeOnX)
-{
+
   win.on('minimize',function(event){ //prevents standard minimize function of a main window
         event.preventDefault()
             win.hide();
     });
   win.on('close', function (event) { //prevents the closing of the aplication when the window closes
+    if (closeOnX)
+    {
+      app.quit();
+    }
+    else {
+
         if( !app.isQuiting){
             event.preventDefault()
             win.hide();
         }
-    });
-}
+
+    }});
+
   tray = new Tray(iconPath); //create a new tray
   var contextMenu = Menu.buildFromTemplate([  //start buliding out the menu for the tray
 
@@ -49,7 +57,7 @@ if (!closeOnX)
               win.show();
           } },
           { label: 'About', click:  function(){ //shows the about window
-            abt = new BrowserWindow({width: 500, height: 570, resizable: false});
+            abt = new BrowserWindow({width: 500, height: 600, resizable: false});
             abt.setMenu(null); //the about window has no menu
             abt.loadURL(url.format({  //loads the webpage for the about window
               pathname: path.join(__dirname, 'about.html'),
@@ -59,14 +67,14 @@ if (!closeOnX)
           } },
           {
             label: 'Preferences', click:  function(){ //shows the about window
-              pref = new BrowserWindow({width: 400, height: 540, resizable: false});
+              pref = new BrowserWindow({width: 400, height: 580, resizable: false});
               pref.setMenu(null); //the about window has no menu
               pref.loadURL(url.format({  //loads the webpage for the about window
                 pathname: path.join(__dirname, 'preferences.html'),
                 protocol: 'file:',
                 slashes: true
               }))
-                // pref.openDevTools();
+                 pref.openDevTools();
             }
           },
           {
@@ -84,11 +92,19 @@ if (!closeOnX)
   tray.setContextMenu(contextMenu); //attach the menu to the tray
 });
 
+function readFile()
+{
+  console.log("Running readfile");
+  mySettings = fs.readFileSync(filePath,'utf8'); //read in the settings file
+  mySettings = (mySettings).split(" "); //split up the settings into an array (each index contains a different setting)
 
+}
 
 function getCloseOnXPref()
 {
-  if (settings.get('closeOnX','true')==="true")
+  readFile();
+  console.log(mySettings[0]);
+  if (mySettings[0]==="true")
   {
     closeOnX = true;
   }
