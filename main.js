a15db46de761b0169b78e37586e915b178fdde98
Alex Epstein <epsteina@wit.edu>
2017-05-14 00:30:16 -0400

    More doublequote fixes
    Former-commit-id: 9fcb41c78e40eebce7517a57f7ed79a4b5d4a02a
    Former-commit-id: 1fa388b72732526be6305840fcb8734c00359542

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const {app, Tray, Menu, shell, BrowserWindow} = require('electron'); //electron application stuff
-const path = require('path'); //allows for use of path
-const url = require('url'); //allows for loadURL and url.format
-const iconPath = path.join(__dirname, 'icon.png'); //grab the icon
-const fs = require('fs');
-const filePath = path.join(__dirname, 'settings.txt');
+const {app, Tray, Menu, shell, BrowserWindow} = require("electron"); //electron application stuff
+const path = require("path"); //allows for use of path
+const url = require("url"); //allows for loadURL and url.format
+const iconPath = path.join(__dirname, "icon.png"); //grab the icon
+const fs = require("fs");
+const filePath = path.join(__dirname, "settings.txt");
 let tray = null; //set the tray to null
 let win = null; //set the main window to null
 let pref = null;
@@ -15,7 +15,7 @@ var mySettings = [];
 function readFile()
 {
   //console.log("Running readfile");
-  mySettings = fs.readFileSync(filePath,'utf8'); //read in the settings file
+  mySettings = fs.readFileSync(filePath,"utf8"); //read in the settings file
   mySettings = (mySettings).split(" "); //split up the settings into an array (each index contains a different setting)
 
 }
@@ -34,13 +34,13 @@ function getCloseOnXPref()
   return;
 }
 
-app.on('ready', function(){
+app.on("ready", function(){
   win = new BrowserWindow({width: 600, height: 475, resizable: false}); //create main window
 
   win.setMenu(null); //the main window had no menu
   win.loadURL(url.format({    //loads the webpage for the main window
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
+    pathname: path.join(__dirname, "index.html"),
+    protocol: "file:",
     slashes: true
   }))
 
@@ -53,11 +53,11 @@ app.on('ready', function(){
 
   getCloseOnXPref();
 
-  win.on('minimize',function(event){ //prevents standard minimize function of a main window
+  win.on("minimize",function(event){ //prevents standard minimize function of a main window
         event.preventDefault()
             win.hide();
     });
-  win.on('close', function (event) { //prevents the closing of the aplication when the window closes
+  win.on("close", function (event) { //prevents the closing of the aplication when the window closes
     if (closeOnX)
     {
       app.quit();
@@ -74,41 +74,41 @@ app.on('ready', function(){
   tray = new Tray(iconPath); //create a new tray
   var contextMenu = Menu.buildFromTemplate([  //start buliding out the menu for the tray
 
-          { label: 'Sandman', click:  function(){  //makes the main window reappear
+          { label: "Sandman", click:  function(){  //makes the main window reappear
               win.show();
           } },
-          { label: 'About', click:  function(){ //shows the about window
+          { label: "About", click:  function(){ //shows the about window
             abt = new BrowserWindow({width: 500, height: 600, resizable: false});
             abt.setMenu(null); //the about window has no menu
             abt.loadURL(url.format({  //loads the webpage for the about window
-              pathname: path.join(__dirname, 'about.html'),
-              protocol: 'file:',
+              pathname: path.join(__dirname, "about.html"),
+              protocol: "file:",
               slashes: true
             }))
           } },
           {
-            label: 'Preferences', click:  function(){ //shows the about window
+            label: "Preferences", click:  function(){ //shows the about window
               pref = new BrowserWindow({width: 400, height: 620, resizable: false});
               pref.setMenu(null); //the about window has no menu
               pref.loadURL(url.format({  //loads the webpage for the about window
-                pathname: path.join(__dirname, 'preferences.html'),
-                protocol: 'file:',
+                pathname: path.join(__dirname, "preferences.html"),
+                protocol: "file:",
                 slashes: true
               }))
                  //pref.openDevTools();
             }
           },
           {
-            label: 'Report a bug...', click:  function(){ //shows the about window
-              shell.openExternal('https://github.com/alexanderepstein/Sandman/issues/new');
+            label: "Report a bug...", click:  function(){ //shows the about window
+              shell.openExternal("https://github.com/alexanderepstein/Sandman/issues/new");
             }
           },
-          { label: 'Quit', click:  function(){ //quit the application
+          { label: "Quit", click:  function(){ //quit the application
               app.isQuiting = true;
               app.quit(); //quit called
 
           } }
       ]);
-  tray.setToolTip('Sandman'); //Honestly no clue but itll make the tray say insomnia in some other place
+  tray.setToolTip("Sandman"); //Honestly no clue but itll make the tray say insomnia in some other place
   tray.setContextMenu(contextMenu); //attach the menu to the tray
 });
