6dc3d5fc8d254c5a98245b5806a783f6761faeb4
Alex Epstein <epsteina@wit.edu>
2017-06-02 03:11:00 -0400

    Added redhat

--- build/linux.sh
+++ build/linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-electron-packager ~/git/Sandman Sandman --platform linux --arch x64 --out ~/git/Sandman/build/dist/ --overwrite  || { echo Error packaging 64 bit application; exit 1; } && electron-installer-debian --src ~/git/Sandman/build/dist/Sandman-linux-x64/ --arch amd64 --config config.json --overwrite || { echo Error packaging 64 bit dpkg; exit 1; } &
 
-electron-packager ~/git/Sandman Sandman --platform linux --arch ia32 --out ~/git/Sandman/build/dist/ --overwrite || { echo Error packaging 32 bit application ; exit 1; } && electron-installer-debian --src ~/git/Sandman/build/dist/Sandman-linux-ia32/ --out ~/git/Sandman/dist/ --arch x86 --config config.json --overwrite || { echo Error packaging 32 bit dpkg; exit 1; }
+
+electron-packager ~/git/Sandman Sandman --platform linux --arch ia32 --out ~/git/Sandman/build/dist/  || { echo Error packaging 32 bit application ; exit 1; } && electron-installer-debian --src ~/git/Sandman/build/dist/Sandman-linux-ia32/  --arch x86 --config config.json  || { echo Error packaging 32 bit dpkg; exit 1; } &
+electron-packager ~/git/Sandman Sandman --platform linux --arch x64 --out ~/git/Sandman/build/dist/   || { echo Error packaging 64 bit application; exit 1; } && electron-installer-debian --src ~/git/Sandman/build/dist/Sandman-linux-x64/ --arch amd64 --config config.json  || { echo Error packaging 64 bit dpkg; exit 1; }
+electron-installer-redhat --src ~/git/Sandman/build/dist/Sandman-linux-x64/ --arch x86_64 --config config.json  || echo Error creating rpm
