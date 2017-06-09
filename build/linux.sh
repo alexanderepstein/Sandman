d93d360d049f85014a437b1c2ab955e1f3d5e2b9
Alex Epstein <epsteina@wit.edu>
2017-06-09 19:24:34 -0400

    Updating automation to 1.7.3

--- build/linux.sh
+++ build/linux.sh
@@ -4,6 +4,6 @@ echo "Resolving dependencies"
 sudo yarn global add electron-packager
 sudo yarn global add electron-installer-debian
 sudo yarn global add electron-installer-redhat
-electron-packager ~/git/Sandman Sandman --platform linux --arch ia32 --out ~/git/Sandman/build/dist/  || { echo Error packaging 32 bit application ; exit 1; } && electron-installer-debian --src ~/git/Sandman/build/dist/Sandman-linux-ia32/  --arch x86 --config config.json  || { echo Error packaging 32 bit dpkg; exit 1; } &
-electron-packager ~/git/Sandman Sandman --platform linux --arch x64 --out ~/git/Sandman/build/dist/   || { echo Error packaging 64 bit application; exit 1; } && electron-installer-debian --src ~/git/Sandman/build/dist/Sandman-linux-x64/ --arch amd64 --config config.json  || { echo Error packaging 64 bit dpkg; exit 1; }
+electron-packager ~/git/Sandman Sandman --platform linux --arch ia32 --electronVersion 1.7.3 --out ~/git/Sandman/build/dist/  || { echo Error packaging 32 bit application ; exit 1; } && electron-installer-debian --src ~/git/Sandman/build/dist/Sandman-linux-ia32/  --arch x86 --config config.json  || { echo Error packaging 32 bit dpkg; exit 1; } &
+electron-packager ~/git/Sandman Sandman --platform linux --arch x64 --electronVersion 1.7.3 --out ~/git/Sandman/build/dist/   || { echo Error packaging 64 bit application; exit 1; } && electron-installer-debian --src ~/git/Sandman/build/dist/Sandman-linux-x64/ --arch amd64 --config config.json  || { echo Error packaging 64 bit dpkg; exit 1; }
 electron-installer-redhat --src ~/git/Sandman/build/dist/Sandman-linux-x64/ --arch x86_64 --config config.json  || echo Error creating rpm
--- build/mac.sh
+++ build/mac.sh
@@ -3,6 +3,6 @@
 echo "Resolving dependencies"
 sudo yarn global add electron-packager
 sudo yarn global add create-dmg
-electron-packager ~/git/Sandman Sandman --platform darwin --arch x64 --out ~/git/Sandman/build/dist/ --electronVersion 1.7.2 --overwrite --icon ~/git/Sandman/assets/sleep.icns || { echo Error packaging  application ; exit 1; } && create-dmg ~/git/Sandman/build/dist/Sandman-darwin-x64/Sandman.app
+electron-packager ~/git/Sandman Sandman --platform darwin --arch x64 --out ~/git/Sandman/build/dist/ --electronVersion 1.7.3 --overwrite --icon ~/git/Sandman/assets/sleep.icns || { echo Error packaging  application ; exit 1; } && create-dmg ~/git/Sandman/build/dist/Sandman-darwin-x64/Sandman.app
 echo Removing temporary files
 sudo rm -r dist
