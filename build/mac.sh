108d9ee5965a45747b2b0629af30b91585391fa7
Alex Epstein <epsteina@wit.edu>
2017-06-09 20:17:34 -0400

    create-dmg needs to be installed without sudo

--- build/mac.sh
+++ build/mac.sh
@@ -2,7 +2,7 @@
 
 echo "Resolving dependencies"
 sudo npm install -g electron-packager
-sudo npm install -g add create-dmg
+npm install -g create-dmg
 electron-packager ~/git/Sandman Sandman --platform darwin --arch x64 --out ~/git/Sandman/build/dist/ --electronVersion 1.7.3 --overwrite --icon ~/git/Sandman/assets/sleep.icns || { echo Error packaging  application ; exit 1; } && create-dmg ~/git/Sandman/build/dist/Sandman-darwin-x64/Sandman.app
 echo Removing temporary files
 sudo rm -r dist
