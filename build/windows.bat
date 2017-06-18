ed0434ed7977b6c1e183d0413dab961c6709f3ec
Alex Epstein <epsteina@wit.edu>
2017-06-18 12:25:55 -0400

    Ignoring tests on build

--- build/windows.bat
+++ build/windows.bat
@@ -1,3 +1,3 @@
 ECHO Running packaging scripts
-CALL electron-packager ../ Sandman --platform win32 --arch x64 --out dist/ --electronVersion 1.7.3 --ignore assets/Usage.gif --ignore scss --ignore build --ignore assets/giphy.gif
-CALL electron-packager ../ Sandman --platform win32 --arch ia32 --out dist/ --electronVersion 1.7.3 --ignore assets/Usage.gif --ignore scss --ignore build --ignore assets/giphy.gif
+CALL electron-packager ../ Sandman --platform win32 --arch x64 --out dist/ --electronVersion 1.7.3 --ignore tests --ignore assets/Usage.gif --ignore scss --ignore build --ignore assets/giphy.gif
+CALL electron-packager ../ Sandman --platform win32 --arch ia32 --out dist/ --electronVersion 1.7.3 --ignore tests --ignore assets/Usage.gif --ignore scss --ignore build --ignore assets/giphy.gif
